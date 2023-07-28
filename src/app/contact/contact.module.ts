cfbe5315ec1dc7a0b2facadfa9c373f748056e3b
Maher Srouji <maher.srouji@saradarbank.com>
2023-07-28 13:54:17 +0300

    ui fixes

--- src/app/contact/contact.module.ts
+++ src/app/contact/contact.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
+import { MainBannerComponent } from './main-banner/main-banner.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
-  declarations: [ContactComponent],
+  declarations: [ContactComponent, MainBannerComponent, ContactFormComponent],
   imports: [CommonModule, ContactRoutingModule],
 })
 export class ContactModule {}
