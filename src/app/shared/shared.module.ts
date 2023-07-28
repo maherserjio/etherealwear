64da52974c1fb359e1fdf40bac0a7cc24f86405e
Maher Srouji <maher.srouji@saradarbank.com>
2023-07-28 12:01:21 +0300

    ui changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from '../components/carousel/carousel.component';
 import { NewsLetterComponent } from '../home/news-letter/news-letter.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [CarouselComponent, NewsLetterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [CarouselComponent, NewsLetterComponent],
 })
 export class SharedModule {}
